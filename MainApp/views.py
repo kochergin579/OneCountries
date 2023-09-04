88bdf77a7adf718a1e07a420883328b5ec5c4a65
Aleksandr <allkor@mail.ru>
2023-09-04 19:49:32 +0300

    modul 3_db

--- MainApp/views.py
+++ MainApp/views.py
@@ -31,5 +31,18 @@ def languages_list(request):
         }
     return render(request, "languages_list.html", context)
 
+def country_page(request, id):
+    try:
+        country = Country.objects.get(id=id)
+        language = Country.Language.all()
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Товар с id {id} не найден")
+    context = {
+        "country": country,
+        "language": language
+    }
+    return render(request, "country_page.html", context)
+
+
 
 
