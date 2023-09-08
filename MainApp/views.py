db92ad2fe84712fea1f33e9ef7deedb347d692fc
Aleksandr <allkor@mail.ru>
2023-09-08 11:22:40 +0300

    modul 3_2_db

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 import json
 from MainApp.models import Country
 from MainApp.models import Language
-from django.shortcuts import render
+
 from django.http import HttpResponseNotFound
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -23,28 +23,25 @@ def countries_list(request):
 
 
 def languages_list(request):
-    with open('countries.json') as f:
-
-        country = json.load(f)
+    language = Language.objects.all()
+    # with open('countries.json') as f:
+    #
+    #     country = json.load(f)
         # for languages in country:
         #     for language in languages.languages:
-        context = {
-            "country": country
+    context = {
+            "language": language
         }
     return render(request, "languages_list.html", context)
 
 def country_page(request, id):
-     try:
-        country = Country.objects.get(id=id)
-        language = Country.Language.all()
-     except ObjectDoesNotExist:
-        return HttpResponseNotFound(f"Товар с id {id} не найден")
-        context = {
-            "country": country,
-            "language": language
-        }
-        return render(request, "country_page.html", context)
-
-
-
-
+     # try:
+    country = Country.objects.get(id=id)
+        # language = Country.Language.all()
+     # except ObjectDoesNotExist:
+     #    return HttpResponseNotFound(f"Товар с id {id} не найден")
+    context = {
+        "country": country
+            # "language": language
+    }
+    return render(request, "country_page.html", context)
